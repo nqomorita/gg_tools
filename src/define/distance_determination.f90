400f08929d0e9eb3cf3d4a7bcbeeac00fe87458e
Naoki MORITA <nqo.morita@gmail.com>
2023-08-30 03:09:28 +0900

    Add get_shapefunc_deriv_3d

--- src/define/distance_determination.f90
+++ src/define/distance_determination.f90
@@ -8,28 +8,26 @@ contains
   !> @ingroup distance
   !> 距離判定関数
   subroutine gg_tools_get_distance_3d(n_base, coord, shape_func, &
-    & global_pos, local_pos, ths, ths_up, is_converge)
+    & global_pos, local_pos, ths, ths_up, eps, is_converge)
     implicit none
-    !>
+    !> 要素を構成する形状関数の個数
     integer(kint), intent(in) :: n_base
-    !>
+    !> 要素を構成する座標
     real(kdouble), intent(in) :: coord(3,n_base)
-    !>
+    !> 形状関数の関数ポインタ
     pointer :: shape_func
-    !>
+    !> 距離判定したい入力座標
     real(kdouble), intent(in) :: global_pos(3)
-    !>
+    !> 入力座標に最も近い局所座標
     real(kdouble), intent(out) :: local_pos(3)
-    !>
+    !> 収束判定閾値
     real(kdouble), intent(in) :: ths
-    !>
+    !> 発散判定閾値
     real(kdouble), intent(in) :: ths_up
-    !>
+    !> 数値微分の差分値
+    real(kdouble), intent(in) :: eps
+    !> 収束判定フラグ
     logical, intent(out) :: is_converge
-    integer(kint) :: i, j
-    real(kdouble) :: jacobi(3,3), invJacob(3,3), det
-    real(kdouble) :: norm, n(n_base), deriv(n_base,3), r(3), dr(3)
-    logical :: is_fail
 
     interface
       subroutine shape_func(n_base, local_pos, weight)
@@ -41,6 +39,11 @@ contains
         real(8), intent(out) :: weight(n_base, 3)
       end subroutine shape_func
     end interface
+
+    integer(kint) :: i, j
+    real(kdouble) :: jacobi(3,3), invJacob(3,3), det
+    real(kdouble) :: norm, n(n_base), deriv(n_base,3), r(3), dr(3)
+    logical :: is_fail
 
     is_converge = .false.
     local_pos = 0.0d0
@@ -55,13 +58,13 @@ contains
         exit
       endif
 
-      call get_shapefunc_deriv(n_base, shape_func, local_pos, deriv)
+      call get_shapefunc_deriv_3d(n_base, shape_func, local_pos, eps, deriv)
 
       jacobi = matmul(coord, deriv)
       call monolis_get_inverse_matrix_R_3d(jacobi, invJacob, det, is_fail)
       if(is_fail) exit
 
-      dr = - Matmul(invJacob, r)
+      dr = - matmul(invJacob, r)
       do j = 1, 3
         local_pos(j) = local_pos(j) + dr(j)
       enddo
@@ -75,15 +78,17 @@ contains
 
   !> @ingroup distance
   !> 距離判定関数
-  subroutine get_shapefunc_deriv(n_base, shape_func, local_pos, deriv)
+  subroutine get_shapefunc_deriv_3d(n_base, shape_func, local_pos, eps, deriv)
     implicit none
-    !>
+    !> 要素を構成する形状関数の個数
     integer(kint), intent(in) :: n_base
-    !>
+    !> 形状関数の関数ポインタ
     pointer :: shape_func
-    !>
+    !> 入力座標に最も近い局所座標
     real(kdouble), intent(in) :: local_pos(3)
-    !>
+    !> 数値微分の差分値
+    real(kdouble), intent(in) :: eps
+    !> 形状関数の微分
     real(kdouble), intent(out) :: deriv(n_base,3)
 
     interface
@@ -97,5 +102,25 @@ contains
       end subroutine shape_func
     end interface
 
-  end subroutine get_shapefunc_deriv
+    integer(kint) :: dim, i
+    real(kdouble) :: pos(3)
+    real(kdouble) :: n1(n_base), n2(n_base), n3(n_base)
+
+    do dim = 1, 3
+      pos = local_pos
+      pos(dim) = pos(dim) - eps
+      call shape_func(n_base, pos, n1)
+
+      pos = local_pos
+      pos(dim) = pos(dim)
+      call shape_func(n_base, pos, n2)
+
+      pos = local_pos
+      pos(dim) = pos(dim) + eps
+      call shape_func(n_base, pos, n3)
+
+      deriv(:,i) = (n1 - 2.0d0*n2 + n3)/(eps*eps)
+    enddo
+  end subroutine get_shapefunc_deriv_3d
+
 end module mod_gg_tools_distance_determination
