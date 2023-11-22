81cc2bfca2ab4fa1cdbff1768c50d80858a53c4e
Naoki MORITA <nqo.morita@gmail.com>
2023-11-22 11:51:49 +0900

    Add getter of ggtools_bucker

--- src/define/def_bucket.f90
+++ src/define/def_bucket.f90
@@ -29,7 +29,7 @@ contains
 
   !> @ingroup bucket
   !> バケットの初期化処理（バケットセルサイズを入力）
-  subroutine ggtools_bucket_init (ggtools_bucket, xmin, xmax, dx)
+  subroutine ggtools_bucket_init(ggtools_bucket, xmin, xmax, dx)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -43,7 +43,7 @@ contains
 
   !> @ingroup bucket
   !> バケットの初期化処理（バケットセル分割数を入力）
-  subroutine ggtools_bucket_init_by_nx (ggtools_bucket, xmin, xmax, nx)
+  subroutine ggtools_bucket_init_by_nx(ggtools_bucket, xmin, xmax, nx)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -57,7 +57,7 @@ contains
 
   !> @ingroup bucket
   !> バケットの終了処理
-  subroutine ggtools_bucket_finalize (ggtools_bucket)
+  subroutine ggtools_bucket_finalize(ggtools_bucket)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -65,7 +65,7 @@ contains
 
   !> @ingroup bucket
   !> バケットセルの初期化処理
-  subroutine ggtools_bucket_cell_init (ggtools_bucket_cell, nx)
+  subroutine ggtools_bucket_cell_init(ggtools_bucket_cell, nx)
     implicit none
     !> バケットセル構造体
     type(type_ggtools_bucket_cell) :: ggtools_bucket_cell(:)
@@ -75,7 +75,7 @@ contains
 
   !> @ingroup bucket
   !> バケットセルの終了処理
-  subroutine ggtools_bucket_cell_finalize (ggtools_bucket_cell)
+  subroutine ggtools_bucket_cell_finalize(ggtools_bucket_cell)
     implicit none
     !> バケットセル構造体
     type(type_ggtools_bucket_cell) :: ggtools_bucket_cell(:)
@@ -83,7 +83,7 @@ contains
 
   !> @ingroup bucket
   !> LBB を指定してバケットセルに id を登録する関数
-  subroutine ggtools_bucket_set_id_by_lbb (ggtools_bucket, ggtools_bucket_cell, xmin_local, xmax_local, id)
+  subroutine ggtools_bucket_set_id_by_lbb(ggtools_bucket, ggtools_bucket_cell, xmin_local, xmax_local, id)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -99,7 +99,7 @@ contains
 
   !> @ingroup bucket
   !> 座標を指定して 1 つのバケットセルに id を登録する関数
-  subroutine ggtools_bucket_set_id_by_point (ggtools_bucket, ggtools_bucket_cell, x_point, id)
+  subroutine ggtools_bucket_set_id_by_point(ggtools_bucket, ggtools_bucket_cell, x_point, id)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -113,7 +113,7 @@ contains
 
   !> @ingroup bucket
   !> 入力した座標を内包するバケットセルから、nid 個の整数配列 id_array を取得する関数
-  subroutine ggtools_bucket_get_by_point (ggtools_bucket, ggtools_bucket_cell, x_point, nid, id_array)
+  subroutine ggtools_bucket_get_by_point(ggtools_bucket, ggtools_bucket_cell, x_point, nid, id_array)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -129,7 +129,7 @@ contains
 
   !> @ingroup bucket
   !> 入力した LBB を内包するバケットセルから、nid 個の整数配列 id_array を取得する関数
-  subroutine ggtools_bucket_get_by_lbb (ggtools_bucket, ggtools_bucket_cell, xmin_local, xmax_local, nid, id_array)
+  subroutine ggtools_bucket_get_by_lbb(ggtools_bucket, ggtools_bucket_cell, xmin_local, xmax_local, nid, id_array)
     implicit none
     !> バケット構造体
     type(type_ggtools_bucket) :: ggtools_bucket
@@ -149,4 +149,36 @@ contains
     !int の min, max の範囲で、バケットセルにアクセスし、個々の n_id_i, id_array_i を取得（ただし実体としては持っていない）
     !n_id_i, id_array_i から重複削除し、n_id, id_array を計算
   end subroutine
+
+  !> @ingroup bucket
+  !> バケットサイズを取得
+  subroutine ggtools_bucket_get_bucket_size(ggtools_bucket, xmin, xmax)
+    implicit none
+    !> バケット構造体
+    type(type_ggtools_bucket) :: ggtools_bucket
+    !> バウンダリボックスの最小座標
+    real(kdouble) :: xmin(3)
+    !> バウンダリボックスの最大座標
+    real(kdouble) :: xmax(3)
+  end subroutine ggtools_bucket_get_bucket_size
+
+  !> @ingroup bucket
+  !> バケットセル分割数を取得
+  subroutine ggtools_bucket_get_number_of_bucket_divisions(ggtools_bucket, nx)
+    implicit none
+    !> バケット構造体
+    type(type_ggtools_bucket) :: ggtools_bucket
+    !> バケットセル分割数（nx, ny, nz）
+    integer(kint) :: nx(3)
+  end subroutine ggtools_bucket_get_number_of_bucket_divisions
+
+  !> @ingroup bucket
+  !> バケットセルサイズを取得
+  subroutine ggtools_bucket_get_bucket_cell_size(ggtools_bucket, dx)
+    implicit none
+    !> バケット構造体
+    type(type_ggtools_bucket) :: ggtools_bucket
+    !> バケットセルサイズ（dx, dy, dz）
+    real(kdouble) :: dx(3)
+  end subroutine ggtools_bucket_get_bucket_cell_size
 end module mod_ggtools_def_bucket
